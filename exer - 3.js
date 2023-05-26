d0e5829e80a04d48b8f74abfbdf571ae1929f10d
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-25 22:53:25 -0600

    Update exer - 3.js

--- exer - 3.js
+++ exer - 3.js
@@ -7,23 +7,3 @@ Object.prototype.styleElement = function (newColor,newSize,newBackground) {
 };
 
 target.styleElement("red","35px","blue");
-
-
-
-// const target = document.getElementById("target");
-
-// function styleElement (element,newColor,newSize,newBackground) {
-//     function NewElement (obj) {
-//         for (let prop in obj) {
-//             this[prop] = obj[prop];
-//         }
-//         this.style.color = newColor;
-//         this.style.fontSize = newSize;
-//         this.style.background = newBackground;
-//     };
-
-//     let newElement = new NewElement(element);
-//     element.parentNode.replaceChild(newElement,element);
-// };
-
-// styleElement(target,"red","35px","blue");
