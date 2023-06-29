06ce33970d241018eb7bddda50b4f2c88e6db30b
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-06-28 18:30:55 -0600

    Update exer5.js

--- exer5.js
+++ exer5.js
@@ -1,35 +1,27 @@
 function counter (str) {
-    if (!(typeof str === "string") && !(typeof str === "number")) return undefined;
-
-    let counter;
-
     if (typeof str === "string") {
         let vowels = str.match(/[aeiou]/gi);
         if (vowels) {
-            counter = vowels.length;
-            return counter
+            return vowels.length
         }
-        else if (Number(str) != 0 && !Number(str)) {
-            counter = 0;
-            return counter
+        else {
+            return 0
         }
     }
-    else {
+    else if (typeof str === "number") {
         str = String(str);
+        let counter = Math.ceil(Math.log10(str));
+        if (counter < str.length) {
+            counter = str.length;
+        };
+        return counter
     };
-
-    counter = Math.ceil(Math.log10(str));
-    if (counter < str.length) {
-        counter = str.length;
-    };
-    
-    return counter
 };
 
 console.log(counter("Hello World")); // EXPECTED: 3
 console.log(counter("bcdfghjklmnpqrstvwxyz")); // EXPECTED: 0
 console.log(counter("1255")); // EXPECTED: 4
-console.log(counter("000")); // EXPECTED: 3
+console.log(counter("000")); // EXPECTED: 0
 console.log(counter(000)); // EXPECTED: 1
 console.log(counter(123)); // EXPECTED: 3
-console.log(counter(null)); // EXPECTED: Invalid input. String/number rquired
+console.log(counter(null)); // EXPECTED: undefined
